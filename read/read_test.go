c07bd32702a5a09311593d2cb268ae06c895a0ac
alapativ2 <vamsi.alapati@anz.com>
2024-07-31 14:51:30 +1000

    refactored read code

--- read/read_test.go
+++ read/read_test.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+    "os"
     "testing"
-    "io/ioutil"
-    "log"
 )
 
 func TestMain(t *testing.T) {
-    data, err := ioutil.ReadFile("../output1.txt")
-    if err != nil {
-        log.Fatalf("failed reading file: %s", err)
+    filePath := "../output1.txt"
+
+    // Check if the file exists
+    if _, err := os.Stat(filePath); os.IsNotExist(err) {
+        t.Fatalf("file does not exist: %s", filePath)
     }
+
+    data, err := os.ReadFile(filePath)
+    if err != nil {
+        t.Fatalf("failed reading file: %s", err)
+    }
+
     t.Log("Contents of output.txt ", string(data))
 }
