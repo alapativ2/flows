f37f8b14b214a395ebc3923f563c60caf0cae15f
alapativ2 <vamsi.alapati@anz.com>
2024-07-31 13:54:33 +1000

    read and dump

---
+++ read/read_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "testing"
+    "io/ioutil"
+    "log"
+)
+
+func TestMain(t *testing.T) {
+    data, err := ioutil.ReadFile("output.txt")
+    if err != nil {
+        log.Fatalf("failed reading file: %s", err)
+    }
+    t.Log("Contents of output.txt ", string(data))
+}
