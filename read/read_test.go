073af66de2ce7045e79f69112ffb1340acf1c7f6
alapativ2 <vamsi.alapati@anz.com>
2024-07-31 14:34:35 +1000

    negative test

--- read/read_test.go
+++ read/read_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestMain(t *testing.T) {
-    data, err := ioutil.ReadFile("../output.txt")
+    data, err := ioutil.ReadFile("../output1.txt")
     if err != nil {
         log.Fatalf("failed reading file: %s", err)
     }
